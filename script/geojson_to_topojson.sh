a4069c0a874f41193771281b0ea4bba33c2893e7
niiyz <yoshida@niiyz.com>
2015-11-30 18:10:53 +0900

    GeoJsonからTopoJsonに一括変更シェル更新。.DS_Store除外。

--- script/geojson_to_topojson.sh
+++ script/geojson_to_topojson.sh
@@ -4,13 +4,17 @@ find ./geojson -type f | while read file
 do
     arr=( `echo $file | tr -s '/' ' '`)
     pref=${arr[2]}
-    # ディレクトリ
+	# ディレクトリ
     dir="topojson/"$pref
     if [ ! -d $dir ]; then
       mkdir -p $dir
     fi
     basename=$(basename $file)
+    if [ $basename = ".DS_Store" ]; then
+      continue
+    fi
     name=( `echo $basename | sed -e "s/\.json/.topojson/"`)
-    #echo $name
+    echo $name
+	# --simplify
     topojson --id-property N03_004 -p NAME=name -p name -o $dir"/"$name geojson/$pref/$basename
 done
